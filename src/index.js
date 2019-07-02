b1e5921df81c4a6da39859b8fc3fa615d07cb03a
Mauricio <magalocr@gmail.com>
2019-07-02 12:39:13 -0600

    Task #2.

--- src/index.js
+++ src/index.js
@@ -14,4 +14,28 @@ function printRandomWords(number) {
 
 printRandomWords(100);
 
-//-----------------------------------------------------
+//-------- Task #2 -------------------------------------
+
+function printRandomWordsFizzBuzz(number) {
+    for (let i = 1; i <= number; i++) {
+        const isFizzBuzz = (i % 3 === 0) && (i % 5 === 0);
+        const isFizz = i % 3 === 0;
+        const isFuzz = i % 5 === 0;
+
+        let text;
+
+        if (isFizzBuzz) {
+            text = 'FizzBuzz';
+        } else if (isFizz) {
+            text = 'Fizz';
+        } else if (isFuzz) {
+            text = `Fuzz`;
+        } else {
+            text = getRandomWordSync();
+        }
+
+        console.log(`${i}. ${text}`);
+    }
+}
+
+printRandomWordsFizzBuzz(100);
