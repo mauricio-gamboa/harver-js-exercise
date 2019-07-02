9ca88fdda829ce1a25cdcbb431479a6f6daa2a10
Mauricio <magalocr@gmail.com>
2019-07-02 12:46:22 -0600

    Task #3.

--- src/index.js
+++ src/index.js
@@ -12,7 +12,8 @@ function printRandomWords(number) {
     }
 }
 
-printRandomWords(100);
+// Uncomment to run
+// printRandomWords(100);
 
 //-------- Task #2 -------------------------------------
 
@@ -38,4 +39,42 @@ function printRandomWordsFizzBuzz(number) {
     }
 }
 
-printRandomWordsFizzBuzz(100);
+// Uncomment to run
+// printRandomWordsFizzBuzz(100);
+
+//-------- Task #3 -------------------------------------
+
+async function printRandomWordsAsync(number) {
+    for (let i = 1; i <= number; i++) {
+        const word = await getRandomWord();
+        console.log(`${i}. ${word}`);
+    }
+}
+
+// Uncomment to run
+// printRandomWordsAsync(100);
+
+async function printRandomWordsFizzBuzzAsync(number) {
+    for (let i = 1; i <= number; i++) {
+        const isFizzBuzz = (i % 3 === 0) && (i % 5 === 0);
+        const isFizz = i % 3 === 0;
+        const isFuzz = i % 5 === 0;
+
+        let text;
+
+        if (isFizzBuzz) {
+            text = 'FizzBuzz';
+        } else if (isFizz) {
+            text = 'Fizz';
+        } else if (isFuzz) {
+            text = `Fuzz`;
+        } else {
+            text = await getRandomWord();
+        }
+
+        console.log(`${i}. ${text}`);
+    }
+}
+
+// Uncomment to run
+// printRandomWordsFizzBuzzAsync(100);
