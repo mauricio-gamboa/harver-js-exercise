fda337770229527371bd18c657e76e2f18e6e718
Mauricio <magalocr@gmail.com>
2019-07-02 12:15:32 -0600

    Task #1.

--- src/index.js
+++ src/index.js
@@ -3,3 +3,15 @@ const { getRandomWordSync, getRandomWord } = require('word-maker');
 console.log('It works!');
 
 // YOUR CODE HERE
+
+//-------- Task #1 -------------------------------------
+
+function printRandomWords(number) {
+    for (let i = 1; i <= number; i++) {
+        console.log(`${i}. ${getRandomWordSync()}`);
+    }
+}
+
+printRandomWords(100);
+
+//-----------------------------------------------------
